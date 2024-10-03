f5af334a1ad09d3dc2fe50b7d913ec256c192fdd
John Russell <john@johnrussell.dev>
2024-10-03 12:15:37 -0600

    Updated the Block interface to properly reflect what should be in all Block classes

--- includes/interfaces/interface-block.php
+++ includes/interfaces/interface-block.php
@@ -9,8 +9,9 @@
 namespace cla_blocks\includes\interfaces;
 
 interface Block {
-	//public function enqueue_block_assets();
-	//public function enqueue_block_editor_assets();
+	public function enqueue_block_assets();
+	public function enqueue_block_editor_assets();
     public function register_block_type();
+	public function render_block($attributes, $content, $block_instance);
     public function render_callback($attributes, $content, $block_instance);
 }
